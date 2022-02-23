bc618ea9f4f3407b6c718c0df0de7817ff08e486
onehousesilver <onehousesilver@naver.com>
2022-02-24 00:03:29 +0900

    finish

--- js/weather.js
+++ js/weather.js
@@ -6,8 +6,10 @@ function onGeoOk(position) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url).then((response) =>
     response.json().then((data) => {
-      const name = data.name;
-      const weather = data.weather[0].name;
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     })
   );
 }
